3461c0bdb9b680c3f6887f473f5ab16cfb5b890d
1Caitlyn1 <caitlyn@gradconnection.com>
2019-01-22 12:09:05 +0200

    Date fix for Safari

--- src/components/Input/Variants/GCDatePicker.js
+++ src/components/Input/Variants/GCDatePicker.js
@@ -78,7 +78,9 @@ class GCDatePicker extends Component {
 
   formatDate (date) {
     const dateObj = new Date(date)
-    return `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`
+    const formatDayForSafari = dateObj.getDate() < 10 ? `0${dateObj.getDate()}` : dateObj.getDate()
+    const formatMonthForSafari = dateObj.getMonth() + 1 < 10 ? `0${dateObj.getMonth() + 1}` : dateObj.getMonth() + 1
+    return `${dateObj.getFullYear()}-${formatMonthForSafari}-${formatDayForSafari}`
   }
 
   render () {
