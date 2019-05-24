5a46931d6ece3031bd91c3ca991822c5517c9760
1Caitlyn1 <caitlyn@gradconnection.com>
2019-05-24 12:32:15 +0200

    Refactor single date picker

--- src/components/Input/Variants/DatePicker.js
+++ src/components/Input/Variants/DatePicker.js
@@ -1,35 +1,61 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import $ from 'jquery';
-import 'daterangepicker';
+import $ from "jquery";
+import "daterangepicker";
+import { GCIcon } from "ui";
+
 class DatePicker extends Component {
-    constructor(props) {
-        super(props);
-        this.pickerRef = null;
-      }
-
-componentDidMount() {
-    this.pickerRef.daterangepicker({
-      singleDatePicker: true,
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.value || this.props.placeholder
+    };
+    this.pickerRef = null;
   }
 
-  render () {
+  componentDidMount() {
+    document.addEventListener('click', this.handleOffClick)
+    this.pickerRef.daterangepicker({
+      singleDatePicker: true
+    }, (date) => {
+      this.setState({value: date.format('l')})
+    })
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('click', this.handleOffClick);
+  };
+
+
+
+  render() {
     return (
-        <div
-          // role='button'
-          ref={picker => {
+      <div
+      ref={picker => {
             this.pickerRef = $(picker);
           }}
-          className='gc-input__el gc-drop-down__value'
-          >
-        {`${this.props.value}`}
+          className={`gc-input__el gc-input__el--no-padding`}>
+      <div
+        role='button'
+        className='gc-drop-down__value'>
+        <input
+          className="gc-drop-down__value__text gc-drop-down__value__text--input"
+          type='text'
+          value={!this.state.value ? '' : this.state.value}
+          placeholder={this.props.placeholder}
+          readOnly
+          />
+        <GCIcon kind='calendarIcon' extendedClassNames='gc-drop-down__caret' />
       </div>
-
-    )
+    </div>
+    );
   }
 }
 
-export {DatePicker}
 
+DatePicker.defaultProps = {
+  placeholder: 'Select Date'
+}
+
+export { DatePicker };
