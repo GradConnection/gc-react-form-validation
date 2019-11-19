f8a900d4acd35e689693fac4e8b2a71a82b67917
CaitlynFowkes <caitlyn@gradconnection.com>
2019-11-19 10:02:29 +0200

    Initial picker implement

--- src/components/Input/Variants/GCDatePicker.js
+++ src/components/Input/Variants/GCDatePicker.js
@@ -7,158 +7,71 @@ import { isEmpty, getDateFromString } from 'utils'
 import Calendar from 'rc-calendar';
 import moment from 'moment';
 
+import DatePicker from 'rc-calendar/lib/Picker';
+
 class GCDatePicker extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      isActive: false,
-      defaultValue: props.defaultValue && new Date(props.defaultValue),
-      changeValue: '',
-      value: props.value && new Date(props.value),
-      min: props.min && new Date(new Date(props.min).setHours(0,0,0,0)),
-      max: props.max && new Date(new Date(props.max).setHours(23,59,59,59))
+      format: 'YYYY-MM-DD'
     }
 
-
-    this.datePicker = React.createRef()
-
-    this.onDropDownClick = this.onDropDownClick.bind(this)
-    this.handleActivateCalendar = this.handleActivateCalendar.bind(this)
-    this.handleOnFocusEffect = this.handleOnFocusEffect.bind(this)
-    this.handleOnBlurEffect = this.handleOnBlurEffect.bind(this)
-  }
-
-  componentDidMount () {
-    if(document)
-    {
-      document.addEventListener('click', this.handleActivateCalendar)
-    }
-  }
-
-  componentWillUnmount () {
-    if(document)
-    {
-      document.removeEventListener('click', this.handleActivateCalendar)
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    if(!prevProps.disabled && this.props.disabled) {
-      this.setState({ isActive: false })
-    }
-    if(prevProps.value !== this.props.value) {
-      this.setState({ isActive: false })
-    }
-  }
-
-  handleActivateCalendar (e) {
-    if(this.datePicker.current) {
-      const touchedMonth = e.target.classList.contains('rc-calendar-month-panel-month')
-      const touchedYear = e.target.classList.contains('rc-calendar-year-panel-year')
-      if (!this.datePicker.current.contains(e.target) && this.state.isActive && !touchedMonth && !touchedYear) {
-        this.setState({ isActive: false }, () => this.props.handleInputValidation(this.props.value))
-      }
-    }
-  }
-
-  handleOnFocusEffect (e) {
-    if(!this.props.disabled) {
-      this.setState({ isActive: true })
-    }
-  }
-
-  handleOnBlurEffect (e) {
-    if (!this.datePicker.current.contains(e.target)) {
-      this.setState({ isActive: false }, () => this.props.handleInputValidation(this.props.value))
-    }
-  }
-
-  onDateSelect (value) {
-    const valueFormatted = this.formatDate(value)
-    this.setState({
-      value: new Date(value),
-    });
-    this.props.onInputChange(valueFormatted)
-  }
-
-  onDropDownClick (e) {
-    e.preventDefault()
-    if(!this.props.disabled){
-      this.setState(state => ({ isActive: !state.isActive }))
-    }
-  }
-
-  formatDate (date) {
-    const makeDateObj = new Date(date)
-    const dateObj = getDateFromString(makeDateObj);
-    const formatDayForSafari = dateObj.getDate() < 10 ? `0${dateObj.getDate()}` : dateObj.getDate()
-    const formatMonthForSafari = dateObj.getMonth() + 1 < 10 ? `0${dateObj.getMonth() + 1}` : dateObj.getMonth() + 1
-    return `${dateObj.getFullYear()}-${formatMonthForSafari}-${formatDayForSafari}`
   }
 
   render () {
-    const { placeholder = 'Select date', defaultValue, min, max, mode } = this.props
-    const { isActive, value } = this.state
+    const { placeholder = 'Select date', disabled = false, value, disabledDates} = this.props
     const dateClasses = classNames('gc-input__el', 'gc-input__el--no-padding', {
-      'gc-input__el--active': isActive
-    })
-
-    const disableDates = (current) => {
-      const currentDateObj = new Date(current)
-      if (min && max) {
-           return (currentDateObj < this.state.min || currentDateObj > this.state.max)
-         }
-         else if (min) {
-           return currentDateObj < this.state.min // cannot select days before min
-         } 
-         else if (max) {
-           return currentDateObj > this.state.max // cannot select days after max
-         }
-         else false;
-      }
-
-    const onChange = (value) => {
-      console.log('DatePicker change: ', (value && value));
+            'gc-input__el--active': false//isActive
+          })
+    const onChange = value => {
+      console.log('onChange value', value);
       this.setState({
-        changeValue: value,
+        value,
       });
+      this.props.onInputChange(value.format('YYYY-MM-DD'))
     }
-
+    // const disableDates = (current) => {
+    //   const currentDateObj = new Date(current)
+    //   if (min && max) {
+    //        return (currentDateObj < this.state.min || currentDateObj > this.state.max)
+    //      }
+    //      else if (min) {
+    //        return currentDateObj < this.state.min // cannot select days before min
+    //      } 
+    //      else if (max) {
+    //        return currentDateObj > this.state.max // cannot select days after max
+    //      }
+    //      else false;
+    //   }
     console.log('this.props', this.props)
     return (
-      <div
-        className={dateClasses}
-        ref={this.datePicker}>
-        <div
-          role='button'
-          className='gc-drop-down__value'
-          onMouseDown={this.onDropDownClick}>
-          <input
-            className='gc-drop-down__value__text gc-drop-down__value__text--input'
-            type='text'
-            value={isEmpty(value) ? '' : this.formatDate(value)}
-            placeholder={placeholder}
-            readOnly
-            onFocus={this.handleOnFocusEffect}
-            onBlur={this.handleOnBlurEffect} />
-          <GCIcon kind='calendarIcon' extendedClassNames='gc-drop-down__caret' />
-        </div>
-        {isActive && (
-          <div className="gc-rc-calendar">
-            <Calendar
-              showDateInput={false}
-              style={{ zIndex: 1000, width: "100%" }}
-              format={'YYYY-MM-DD'}
-              disabledDate={current => disableDates(current)}
-              onChange={(v) => onChange(v)}
-              onSelect={(v) => this.onDateSelect(v)}
-              selectedValue={value ? moment(value) : ''}
-              dateInputPlaceholder={placeholder}
-              defaultValue={defaultValue && moment(defaultValue)}
-              />
-          </div>
-        )}
-      </div>
+      <DatePicker
+      animation="slide-up"
+      value={this.props.value ? moment(this.props.value) : ''}
+      onChange={onChange}
+      // disabled={disabled}
+      calendar={<Calendar
+        format={this.state.format}
+      />}
+    >
+      {
+        ({ value }) => {
+          return (
+            <div className={dateClasses}>
+              {console.log('value in component', value)}
+                <input
+                  placeholder={placeholder}
+                  disabled={disabled}
+                  readOnly
+                  value={value ? moment(new Date(value)).format(this.state.format) : ''}
+                  className='gc-drop-down__value__text gc-drop-down__value__text--input'
+                />
+                 <GCIcon kind='calendarIcon' extendedClassNames='gc-drop-down__caret' />
+                </div>
+          );
+        }
+      }
+    </DatePicker>
     )
   }
 }
